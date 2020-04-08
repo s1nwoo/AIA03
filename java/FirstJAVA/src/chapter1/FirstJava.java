014780a93156c73563e1fb098c9820cce4717f6d
s1nwoo <tizm7777@naver.com>
2020-04-08 18:22:30 +0900

    Update FirstJava.java
    주석을 추가했어요

--- java/FirstJAVA/src/chapter1/FirstJava.java
+++ java/FirstJAVA/src/chapter1/FirstJava.java
@@ -1,12 +1,18 @@
+/*
+ * 파일이름: FirstJava
+ * 작성자: 정신우
+ * 작성일: 2020년 4월 08일
+ * 작성이유: System.out.println 메소드 기능 테스트
+ */
 package chapter1;
 
 public class FirstJava {
 
 	public static void main(String[] args) {
-
+	
 		// "Hello JAVA! 를 출력하세요.	
 		System.out.println("Hello JAVA!");
-
+			
 	}
 
 }
