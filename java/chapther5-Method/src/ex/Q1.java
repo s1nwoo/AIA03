34f9b219538980edfdbe8bc1c30e1baf0f2a0ba1
s1nwoo <tizm7777@naver.com>
2020-04-22 17:40:32 +0900

    메서드

---
+++ java/chapther5-Method/src/ex/Q1.java
@@ -0,0 +1,28 @@
+package ex;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+
+//Q1. 두 개의 정수를 전달받아서, 두수의 사칙연산 결과를 출력하는 메서드와 이 메서드를 호출하는 main메서드를 정의해보자.
+//    단, 나눗셈은 몫과 나머지를 각각 출력해야 한다.		
+		
+		value(20,10);
+		
+	}
+		static void value(int a, int b) {
+			int add = a + b;
+			int min = a - b;
+			int mul = a * b;
+			int div = a / b;
+			int rem = a % b;;
+			
+			System.out.println(a+" + "+b+" = " +add);
+			System.out.println(a+" - "+b+" = " +min);
+			System.out.println(a+" * "+b+" = " +mul);
+			System.out.println(a+" / "+b+" = "+"몫: " +div+" 나머지: "+rem);
+			
+		}
+		
+	}
+
---
+++ java/chapther5-Method/src/ex/Q2.java
@@ -0,0 +1,11 @@
+package ex;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
