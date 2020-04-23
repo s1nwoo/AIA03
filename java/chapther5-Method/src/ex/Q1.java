8d9c5f1e2bfc6286bf0dedbe6d9448f8e02c705f
s1nwoo <tizm7777@naver.com>
2020-04-23 12:33:55 +0900

    메서드 문제 풀이

--- java/chapther5-Method/src/ex/Q1.java
+++ java/chapther5-Method/src/ex/Q1.java
@@ -7,9 +7,12 @@ public class Q1 {
 //Q1. 두 개의 정수를 전달받아서, 두수의 사칙연산 결과를 출력하는 메서드와 이 메서드를 호출하는 main메서드를 정의해보자.
 //    단, 나눗셈은 몫과 나머지를 각각 출력해야 한다.		
 		
-		value(20,10);
 		
+	// value 메서드에 a값과 b값을 전달한다.
+		value(20,10);	
 	}
+	
+	// 두 수의 사칙연산 결과를 출력하는 메서드 선언
 		static void value(int a, int b) {
 			int add = a + b;
 			int min = a - b;
