edcdb76512d529f121124f3cbf79592b66ac3ac8
s1nwoo <tizm7777@naver.com>
2020-04-23 12:33:40 +0900

    상속자

---
+++ java/chapther10-Inheritance/src/car/CarMian.java
@@ -0,0 +1,15 @@
+package car;
+
+public class CarMian {
+
+	public static void main(String[] args) {
+
+		// 클래스 호출
+		HybridWaterCar result = new HybridWaterCar(100, 100, 100);
+		// 출력 메서드 호출
+		result.showCurrentGauge();
+		
+		
+	}
+
+}
