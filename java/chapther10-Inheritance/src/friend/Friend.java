c319176722189a8ed9caec1ad305def750666eef
s1nwoo <tizm7777@naver.com>
2020-04-28 20:31:59 +0900

    상속-인스턴스

--- java/chapther10-Inheritance/src/friend/Friend.java
+++ java/chapther10-Inheritance/src/friend/Friend.java
@@ -6,27 +6,30 @@ package friend;
  		기본 속성과 기능을 구성
  */
 
-public class Friend {
+public abstract class Friend implements Info {
 
 	String name;		// 친구의 이름
 	String phoneNum;	// 친구의 전화번호
 	String addr;		// 친구의 주소
 	
 	// 생성자
-	public Friend(String name, String phoneNum, String addr) {
+	Friend(String name, String phoneNum, String addr) {
 		this.name = name;
 		this.phoneNum = phoneNum;
 		this.addr = addr;
 	}
 	
-	public void showBasicInfo() {
+	void showBasicInfo() {
 		System.out.println("이름: "+name);
 		System.out.println("번호: "+phoneNum);
 		System.out.println("주소: "+addr);
 		
 	}
 	// 상속의 목적으로 오버라이딩할 메서드
-	public void showData() {
+//	void showData() {
 		// 기본데이터와 하위클래스의 데이터를 출력하도록 오버라이딩한다.
-	}
+//	}
+	
+	// 추상메서드 생성
+	// abstract void showData(){}
 }
