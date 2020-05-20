c0248912e613c3e4b05146922134c62794ae183e
s1nwoo <tizm7777@naver.com>
2020-05-20 17:47:26 +0900

    05.19 과제

--- SQL/SelectStudy.sql
+++ SQL/SelectStudy.sql
@@ -1,3 +1,6 @@
+-- select 문을 이용해서 요구하는 sql문 작성
+
+
 -- 1. 덧셈연산자를 이용하여 모든 사원에 대해서 $300의 급여 인상을 계산한 후 사원의 이름, 급여, 인상된 급여를 출력하시오.
 SELECT ename, sal, sal+500
 from emp;
@@ -76,3 +79,65 @@ WHERE JOB IN('CLERK','SALESMAN') AND NOT SAL IN(1600, 950, 1300);
 SELECT ENAME, SAL, COMM
 FROM EMP
 WHERE COMM >= 500;
+
+-- 추가문제1) 마당서점의 고객이 요구하는 다음질문에 대해 SQL문을 작성하시오.
+ 
+SELECT * FROM book;
+SELECT * FROM Customer;
+SELECT * FROM Orders;
+
+--(1) 도서번호가 1인 도서의 이름
+SELECT BOOKNAME
+FROM BOOK
+WHERE BOOKID=1;
+ 
+--(2) 가격이 20,000원 이상인 도서의 이름
+SELECT BOOKNAME
+FROM BOOK
+WHERE PRICE >= 20000;
+ 
+--(3) 박지성의 총구매액(박지성의 고객번호는 1번으로 놓고 작성)
+SELECT SUM(SALEPRICE)
+FROM orders
+WHERE custid=1;
+ 
+--(4) 박지성이 구매한 도서의 수(박지성의 고객번호는 1번으로 놓고 작성)
+SELECT COUNT(ORDERID)
+FROM orders
+WHERE custid=1;
+ 
+-- 추가문제2) 마당서점의 운영자와 경영자가 요구하는 다음 질문에 대해 SQL문을 작성하시오.
+ 
+--(1) 마당서점 도서의 총개수
+SELECT COUNT(BOOKID)
+FROM book;
+ 
+--(2) 마당서점에 도서를 출고하는 출판사의 총개수
+SELECT COUNT(DISTINCT PUBLISHER)
+FROM BOOK
+WHERE PUBLISHER IS NOT NULL;
+ 
+--(3) 모든고객의 이름, 주소
+SELECT NAME, ADDRESS
+FROM Customer;
+ 
+--(4) 2014년 7월4일~7월7일 사이에 주문받은 도서의 주문번호
+SELECT ORDERID
+FROM ORDERS
+WHERE ORDERDATE BETWEEN '2014-07-04' AND '2014-07-07';
+ 
+--(5) 2014년 7월4일~7월7일 사이에 주문받은 도서를 제외한 도서의 주문번호
+SELECT ORDERID
+FROM ORDERS
+WHERE NOT ORDERDATE BETWEEN '2014-07-04' AND '2014-07-07';
+ 
+--(6) 성이 ‘김’씨인 고객의 이름과 주소
+SELECT NAME, ADDRESS
+FROM CUSTOMER
+WHERE NAME LIKE '%김';
+ 
+-- (7) 성이 ‘김’씨이고 이름이 ‘아’로 끝나는 고객의 이름과 주소
+SELECT NAME, ADDRESS
+FROM CUSTOMER
+WHERE NAME LIKE '김%' AND NAME LIKE '%아';
+
