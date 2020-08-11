ba33f479ed94ce5b825c894f0de9a5505f3989d7
s1nwoo <tizm7777@naver.com>
2020-08-11 20:22:27 +0900

    spring 암호화

---
+++ spring_project/EncriptTest/src/encrypt/ExTestMain.java
@@ -0,0 +1,52 @@
+package encrypt;
+
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+
+public class ExTestMain {
+
+	public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException, GeneralSecurityException {
+
+		String str = "hello java";
+		
+		// 단방향 암호화 객체 생성 : SHA-256
+		Sha256 sha256= new Sha256();
+		
+		String enStr = sha256.encrypt(str);
+		
+		System.out.println("평문 : " +str);
+		System.out.println("SHA-256암호화: " +enStr);
+		System.out.println(sha256.encrypt("1"));
+		System.out.println(sha256.encrypt("1234567890abcdef"));
+		System.out.println(sha256.encrypt("1").length());
+		
+		
+		// 양방항 암호화 객체 : AES
+		// key : 16자리
+		String key = "0000000000000001";
+		String key2 = "0000000000000002";
+		AES256Util aes256Util = new AES256Util(key);
+		AES256Util aes256Util2 = new AES256Util(key2);
+		
+		System.out.println("===========================================");
+		
+		String encStr1 = aes256Util.encrypt(str);
+		String encStr2 = aes256Util2.encrypt(str);
+		
+		// 암호화
+		System.out.println("평문: "+str);
+		System.out.println("AES-256 암호화 : " + encStr1);
+		System.out.println("AES-256 암호화 : " + encStr2);
+		
+		System.out.println("===========================================");
+		
+		String plainStr1 = aes256Util.decrypt(encStr1);
+		String plainStr2 = aes256Util2.decrypt(encStr2);
+		System.out.println("AES-256 복호화 : " +plainStr1);
+		System.out.println("AES-256 복호화 : " +plainStr2);
+		
+		
+	}
+
+}
--- spring_project/Spring-OP/src/main/java/com/aia/op/member/controller/MemberRegController.java
+++ spring_project/Spring-OP/src/main/java/com/aia/op/member/controller/MemberRegController.java
@@ -1,8 +1,13 @@
 package com.aia.op.member.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.aia.op.member.model.Member;
 import com.aia.op.member.model.MemberRegRequest;
 import com.aia.op.member.service.MemberRegService;
+import com.aia.op.util.AES256Util;
+import com.aia.op.util.Sha256;
 
 @Controller
 @RequestMapping("/member/memberReg")
@@ -19,6 +26,16 @@ public class MemberRegController {
 	@Autowired
 	MemberRegService regService;
 
+	@Autowired
+	private Sha256 sha256;
+	
+	@Autowired
+	private AES256Util aes256Util; 
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String getMemberRegForm() {
 		return "member/memberRegForm";
@@ -30,12 +47,29 @@ public class MemberRegController {
 			/* Member member, */
 			HttpServletRequest request,
 			Model model
-			) {
+			) throws NoSuchAlgorithmException, UnsupportedEncodingException, GeneralSecurityException {
 		
 		System.out.println("controller : "+regRequest);
+		
+		System.out.println("SHA 암호화");
+		System.out.println(sha256.encrypt(regRequest.getUpw()));
+		System.out.println(sha256.encrypt(regRequest.getUpw()).length());
+		
+		System.out.println("-----------------------------------------------");
+		
+		System.out.println("AES 암호화");
+		System.out.println(aes256Util.encrypt(regRequest.getUpw()));
+		System.out.println(aes256Util.encrypt(regRequest.getUpw()).length());
+		
+		System.out.println("-----------------------------------------------");
+		System.out.println("Spring Security 암호화");
+		System.out.println(passwordEncoder.encode(regRequest.getUpw()));
+		System.out.println(passwordEncoder.encode(regRequest.getUpw()).length());
+		
+		
 		/* System.out.println("member : "+member); */
 		
-		model.addAttribute("result", regService.memberReg(regRequest, request));
+		// model.addAttribute("result", regService.memberReg(regRequest, request));
 		
 		return "member/memberReg";
 	}
